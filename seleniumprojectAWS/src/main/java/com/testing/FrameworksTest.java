95b163de4d3865ef3c2a99b60cd7361fc43263b9
SuvarchalaJaldu <jsuvarchala4@gmail.com>
2023-05-22 23:47:34 +0200

    initial commit

---
+++ seleniumprojectAWS/src/main/java/com/testing/FrameworksTest.java
@@ -0,0 +1,74 @@
+package com.testing;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FrameworksTest {
+	WebDriver driver;
+
+@BeforeMethod
+@Parameters("browser")
+public void setup(String browser)
+{
+	if(browser.equalsIgnoreCase("chrome")) {
+		WebDriverManager.chromedriver().setup();
+		//driver = new ChromeDriver();
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("browserName", "chrome");
+		try {
+			driver = new RemoteWebDriver(new URL("http://54.221.30.22:4444/wd/hub"), cap);
+		}catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	else if(browser.equalsIgnoreCase("firefox")) {
+		WebDriverManager.chromedriver().setup();
+		//driver = new FirefoxDriver();
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("browserName", "firefox");
+		try {
+			driver = new RemoteWebDriver(new URL("http://54.221.30.22:4444/wd/hub"), cap);
+		}catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.get("https://www.freshworks.com/");
+}
+
+	@Test
+	public void FreshWorksTitleTest() {
+		System.out.println(driver.getTitle());
+		AssertJUnit.assertEquals(driver.getTitle(), "Innovative Software for Business Needs | Freshworks");
+	}
+
+	@Test
+	public void getFooterLinksTest() {
+		List<WebElement> footerLinksList = driver.findElements(By.cssSelector("#__next > footer > div > div:nth-child(2) > nav"));
+		footerLinksList.forEach(ele -> System.out.println(ele.getText()));
+		//AssertJUnit.assertEquals(footerLinksList.size(),35);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
